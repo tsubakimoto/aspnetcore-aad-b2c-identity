50e3a1947fb9be00defbf4219767a8ab6d9d161a
tsubakimoto <tkmbmbr@gmail.com>
2018-04-01 17:04:01 +0900

    [add][modify] AAD B2C GraphApi CreateUser

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json;
 using WebApplication1.Models;
+using WebApplication1.Models.AccountViewModels;
 
 namespace WebApplication1.Controllers
 {
@@ -19,6 +23,7 @@ namespace WebApplication1.Controllers
         private readonly string clientSecret;
         private readonly AuthenticationContext authContext;
         private readonly ClientCredential credential;
+        private readonly string userPassword;
 
         public HomeController(IConfiguration configuration)
         {
@@ -27,6 +32,7 @@ namespace WebApplication1.Controllers
             clientSecret = configuration["ClientSecret"];
             authContext = new AuthenticationContext($"https://login.microsoftonline.com/{tenant}");
             credential = new ClientCredential(clientId, clientSecret);
+            userPassword = configuration["UserPassword"];
         }
 
         public IActionResult Index()
@@ -59,7 +65,7 @@ namespace WebApplication1.Controllers
             return result.AccessToken;
         }
 
-        private async Task<string> SendGraphGetRequest(string api, string query = null)
+        private async Task<string> SendGraphGetRequestAsync(string api, string query = null)
         {
             var http = new HttpClient();
             var url = $"https://graph.windows.net/{tenant}{api}?api-version=1.6";
@@ -76,9 +82,61 @@ namespace WebApplication1.Controllers
             return await response.Content.ReadAsStringAsync();
         }
 
+        private async Task<string> SendGraphPostRequestAsync(string api, string json)
+        {
+            // NOTE: This client uses ADAL v2, not ADAL v4
+            var accessToken = await GetTokenAsync();
+            HttpClient http = new HttpClient();
+            var url = $"https://graph.windows.net/{tenant}{api}?api-version=1.6";
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await http.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                string error = await response.Content.ReadAsStringAsync();
+                object formatted = JsonConvert.DeserializeObject(error);
+                throw new WebException("Error Calling the Graph API: \n" + JsonConvert.SerializeObject(formatted, Formatting.Indented));
+            }
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
         public async Task<IActionResult> GetUsers()
         {
-            var result = await SendGraphGetRequest("/users");
+            var result = await SendGraphGetRequestAsync("/users");
+            return Content(result);
+        }
+
+        public async Task<IActionResult> CreateUser()
+        {
+            var name = $"yuta{DateTime.Now.Ticks}";
+            var email = $"{name}@example.com";
+            var user = new AadB2cUser
+            {
+                accountEnabled = true,
+                signInNames = new Signinname[]
+                {
+                    new Signinname { type = "emailAddress", value = email }
+                },
+                creationType = "LocalAccount",
+                displayName = name,
+                mailNickname = name,
+                passwordProfile = new Passwordprofile
+                {
+                    password = userPassword,
+                    forceChangePasswordNextLogin = false
+                },
+                passwordPolicies = "DisablePasswordExpiration",
+                city = "Fukuoka",
+                givenName = "Yuta",
+                postalCode = "000-0000",
+                state = "Fukuoka",
+                surname = "Matsumura",
+            };
+            var result = await SendGraphPostRequestAsync("/users", JsonConvert.SerializeObject(user));
             return Content(result);
         }
     }
---
+++ WebApplication1/Models/AccountViewModels/AadB2cUser.cs
@@ -0,0 +1,38 @@
+﻿namespace WebApplication1.Models.AccountViewModels
+{
+    public class AadB2cUser
+    {
+        public bool accountEnabled { get; set; } = true;
+        public Signinname[] signInNames { get; set; }
+        public string creationType { get; set; } = "LocalAccount";
+        public string displayName { get; set; } = "";
+        public string mailNickname { get; set; } = "";
+        public Passwordprofile passwordProfile { get; set; }
+        public string passwordPolicies { get; set; } = "DisablePasswordExpiration";
+        public string city { get; set; } = "";
+        public object country { get; set; } = null;
+        public object facsimileTelephoneNumber { get; set; } = null;
+        public string givenName { get; set; } = "";
+        public object mail { get; set; } = null;
+        public object mobile { get; set; } = null;
+        public object[] otherMails { get; set; } = new object[] { };
+        public string postalCode { get; set; } = "";
+        public object preferredLanguage { get; set; } = null;
+        public string state { get; set; } = "";
+        public object streetAddress { get; set; } = null;
+        public string surname { get; set; } = "";
+        public object telephoneNumber { get; set; } = null;
+    }
+
+    public class Passwordprofile
+    {
+        public string password { get; set; } = "";
+        public bool forceChangePasswordNextLogin { get; set; } = false;
+    }
+
+    public class Signinname
+    {
+        public string type { get; set; } = "emailAddress";
+        public string value { get; set; } = "";
+    }
+}
